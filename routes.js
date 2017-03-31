966a6a4fe487acfdbf7683c1c6979fb11b2c43f1
svillegasc <santivicastro18@gmail.com>
2017-03-31 16:15:44 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,10 +5,14 @@ const products = db.get('products')
 
 
 class ContactRouter extends Router {
+    // Services Products
     get routes() {
         return {
             '/:Products': [
                 new Route("get", "index"),
+                new Route("post", "indexPost"),
+                new Route("update", "indexUpd"),
+                new Route("delete", "indexDel"),
             ]
         };
     }
@@ -19,6 +23,74 @@ class ContactRouter extends Router {
         products.find({},function(e,result){
            res.json(result); 
         });
+    }
+
+    indexPost(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código: ${person.codigo} 
+                  Nombre: ${person.nombre}
+                  Valor: ${person.valor}
+                  Descripcion: ${person.descripcion}`);
+    }
+
+    indexUpd(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código: ${person.codigo} 
+                  Nombre: ${person.nombre}
+                  Valor: ${person.valor}
+                  Descripcion: ${person.descripcion}`);
+    }
+
+    indexDel(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código: ${person.codigo}`);
+    }
+
+    // Services Pedidos
+    get routes() {
+        return {
+            '/:Pedidos': [
+                new Route("get", "index"),
+                new Route("post", "indexPost"),
+                new Route("update", "indexUpd"),
+                new Route("delete", "indexDel"),
+            ]
+        };
+    }
+
+    index(req, res) {
+      //  var listproducts = products.index();
+      console.log(bonito.find());
+        bonito.find({},function(e,result){
+           res.json(result); 
+        });
+    }
+
+    indexPost(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código Pedido: ${person.codigoPedido} 
+                  Nombre: ${person.nombre}
+                  Dirección: ${person.direccion}
+                  Teléfono: ${person.telefono}`);
+    }
+
+    indexUpd(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código Pedido: ${person.codigoPedido} 
+                  Nombre: ${person.nombre}
+                  Dirección: ${person.direccion}
+                  Teléfono: ${person.telefono}`);
+    }
+
+    indexDel(req, res) {
+        var person = req.body;
+        console.log(person);
+        res.send(`Código Pedido: ${person.codigoPedido}`);
     }
 };
 
